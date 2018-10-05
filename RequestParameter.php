1a92408564678bf154c8022eb257f93873c971b7
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2018-10-06 00:28:44 +0300

    Updated license header

--- RequestParameter.php
+++ RequestParameter.php
@@ -3,7 +3,7 @@
 /* 
  * The MIT License
  *
- * Copyright 2018 Ibrahim.
+ * Copyright 2018 Ibrahim BinAlshikh, rest-easy (v1.4.2).
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
