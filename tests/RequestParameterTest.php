5a9023dc35f1e66276614fc54767cdbf4f61100d
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-15 14:44:04 +0300

    Create RequestParameterTest.php

---
+++ tests/RequestParameterTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace restEasy\tests;
+use PHPUnit\Framework\TestCase;
+use restEasy\RequestParameter;
+/**
+ * Description of RequestParameterTest
+ *
+ * @author Eng.Ibrahim
+ */
+class RequestParameterTest extends TestCase{
+    /**
+     * @test
+     */
+    public function testConstructor00() {
+        $this->assertTrue(true);
+    }
+}
