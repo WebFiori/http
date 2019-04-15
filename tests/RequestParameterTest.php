e03feb49201ced1b9bdcc807c8b0909ae94bc839
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-16 02:46:37 +0300

    Update RequestParameterTest.php
    Added tests to test the method RequestParameter::setIsEmptyStringAllowed()

--- tests/RequestParameterTest.php
+++ tests/RequestParameterTest.php
@@ -83,4 +83,43 @@ class RequestParameterTest extends TestCase{
         $this->assertNull($requestParam->getCustomFilterFunction());
         $this->assertEquals('integer',$requestParam->getType());
     }
+    /**
+     * @test
+     */
+    public function testConstructor06() {
+        $requestParam = new RequestParameter('hello','unkown');
+        $this->assertEquals('hello',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertFalse($requestParam->isOptional());
+        $this->assertNull($requestParam->getMaxVal());
+        $this->assertNull($requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('string',$requestParam->getType());
+    }
+    /**
+     * @test
+     */
+    public function testSetIsEmptyStrAllowed00() {
+        $requestParam = new RequestParameter('hello');
+        $this->assertTrue($requestParam->setIsEmptyStringAllowed(true));
+        $this->assertTrue($requestParam->isEmptyStringAllowed());
+        $this->assertTrue($requestParam->setIsEmptyStringAllowed(false));
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+    }
+    /**
+     * @test
+     */
+    public function testSetIsEmptyStrAllowed01() {
+        $requestParam = new RequestParameter('hello','integer');
+        $this->assertFalse($requestParam->setIsEmptyStringAllowed(true));
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $requestParam->setType('string');
+        $this->assertTrue($requestParam->setIsEmptyStringAllowed(true));
+        $this->assertTrue($requestParam->isEmptyStringAllowed());
+        $requestParam->setType('integer');
+        $this->assertFalse($requestParam->setIsEmptyStringAllowed(false));
+        $this->assertTrue($requestParam->isEmptyStringAllowed());
+    }
 }
