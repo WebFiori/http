8b445d89e2091fa966e43cf1ed620e7958125070
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-15 15:28:33 +0300

    Update RequestParameterTest.php
    Added tests to test the constructor of the class.

--- tests/RequestParameterTest.php
+++ tests/RequestParameterTest.php
@@ -12,6 +12,75 @@ class RequestParameterTest extends TestCase{
      * @test
      */
     public function testConstructor00() {
-        $this->assertTrue(true);
+        $requestParam = new RequestParameter('');
+        $this->assertEquals('a-parameter',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertFalse($requestParam->isOptional());
+        $this->assertNull($requestParam->getMaxVal());
+        $this->assertNull($requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('string',$requestParam->getType());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor01() {
+        $requestParam = new RequestParameter('invalid name');
+        $this->assertEquals('a-parameter',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertFalse($requestParam->isOptional());
+        $this->assertNull($requestParam->getMaxVal());
+        $this->assertNull($requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('string',$requestParam->getType());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor02() {
+        $requestParam = new RequestParameter('   valid-name-1-2-BUT-not-trimmed    ');
+        $this->assertEquals('valid-name-1-2-BUT-not-trimmed',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertFalse($requestParam->isOptional());
+        $this->assertNull($requestParam->getMaxVal());
+        $this->assertNull($requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('string',$requestParam->getType());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor03() {
+        $requestParam = new RequestParameter('valid','integer',true);
+        $this->assertEquals('valid',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertTrue($requestParam->isOptional());
+        $this->assertEquals(PHP_INT_MAX, $requestParam->getMaxVal());
+        $this->assertEquals(~PHP_INT_MAX,$requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('integer',$requestParam->getType());
+    }
+    /**
+     * @test
+     */
+    public function testConstructor04() {
+        $requestParam = new RequestParameter('valid',' INteger ',true);
+        $this->assertEquals('valid',$requestParam->getName());
+        $this->assertFalse($requestParam->isEmptyStringAllowed());
+        $this->assertTrue($requestParam->isOptional());
+        $this->assertEquals(PHP_INT_MAX, $requestParam->getMaxVal());
+        $this->assertEquals(~PHP_INT_MAX,$requestParam->getMinVal());
+        $this->assertNull($requestParam->getDefault());
+        $this->assertNull($requestParam->getDescription());
+        $this->assertNull($requestParam->getCustomFilterFunction());
+        $this->assertEquals('integer',$requestParam->getType());
     }
 }
