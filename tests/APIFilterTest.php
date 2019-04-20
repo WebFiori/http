9b57bcad1855177d6eda44e2b6ea357601bbe5ac
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2019-04-20 19:59:54 +0300

    Create APIFilterTest.php

---
+++ tests/APIFilterTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace restEasy\tests;
+use PHPUnit\Framework\TestCase;
+use restEasy\RequestParameter;
+use restEasy\APIFilter;
+/**
+ * Description of APIFilterTest
+ *
+ * @author Eng.Ibrahim
+ */
+class APIFilterTest {
+    /**
+     * @test
+     */
+    public function constructorTest00() {
+        $this->assertTrue(true);
+    }
+}
