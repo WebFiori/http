fc62d4e1c85cabe27e5d3337384e427d7b8dbce3
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-09-16 22:49:48 +0300

    Create TestServiceObj.php

---
+++ tests/TestServiceObj.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace restEasy\tests;
+
+use webfiori\restEasy\WebService;
+/**
+ * Description of TestServiceObj
+ *
+ * @author Ibrahim
+ */
+class TestServiceObj extends WebService {
+    public function __construct($name) {
+        parent::__construct($name);
+    }
+    //put your code here
+    public function isAuthorized() {
+        
+    }
+
+    public function processRequest($inputs) {
+        
+    }
+
+}
