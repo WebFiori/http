008bef06b17438ec80ab76f051a54cadd23da144
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-09-24 13:38:43 +0300

    Create loader.php

---
+++ examples/loader.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../vendor/webfiori/jsonx/src/Json.php';
+require '../vendor/webfiori/jsonx/src/JsonI.php';
+require '../src/APIFilter.php';
+require '../src/ParamTypes.php';
+require '../src/RequestParameter.php';
+require '../src/AbstractWebService.php';
+require '../src/WebServicesManager.php';
